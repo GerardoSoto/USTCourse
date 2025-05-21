d96f29513fe8d3eb1b3c8d4df024f771a85f7b29
Gerardo Soto <soto1187@gmail.com>
2025-05-21 12:38:27 -0700

    Lab 12: Hiding and Showing Components

--- Day-7/projectpilot/src/projects/ProjectList.tsx
+++ Day-7/projectpilot/src/projects/ProjectList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
@@ -15,16 +16,27 @@ function ProjectList({ projects }: ProjectListProps) {
   //     ))}
   //   </ul>
   // );
+
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+
   const handleEdit = (project: Project) => {
     console.log(project);
+    setProjectBeingEdited(project);
   }
 
   return (
     <div className="row">
       {projects.map((project) => (
         <div key={project.id} className="cols-sm">
-          <ProjectCard project={project}  onEdit={handleEdit}/>
-          <ProjectForm />
+          {/* <ProjectCard project={project}  onEdit={handleEdit}/>
+          <ProjectForm /> */}
+           { 
+            project === projectBeingEdited ? (
+              <ProjectForm />
+            ) :
+            (
+              <ProjectCard project={project}  onEdit={handleEdit}/>
+            )}
         </div>
       ))}
     </div>
