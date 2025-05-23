9608851f124c722f2fe8b43a50c54d89d6834403
Gerardo Soto <soto1187@gmail.com>
2025-05-23 12:07:11 -0700

    Lab 18: HTTP GET

--- Day-7/projectpilot/src/projects/ProjectsPage.tsx
+++ Day-7/projectpilot/src/projects/ProjectsPage.tsx
@@ -1,25 +1,90 @@
-import { useState } from "react";
-import { MOCK_PROJECTS } from "./MockProjects";
+import React, { useState, useEffect } from "react";
+// import { MOCK_PROJECTS } from "./MockProjects";
+import { projectAPI } from "./projectAPI";
 import ProjectList from "./ProjectList";
 import { Project } from "./Project";
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  //const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  // Approach 1: using promise then
+  //  useEffect(() => {
+  //    setLoading(true);
+  //    projectAPI
+  //      .get(1)
+  //      .then((data) => {
+  //        setError(null);
+  //        setLoading(false);
+  //        setProjects(data);
+  //      })
+  //      .catch((e) => {
+  //        setLoading(false);
+  //        setError(e.message);
+  //        if (e instanceof Error) {
+  //           setError(e.message);
+  //        }
+  //      });
+  //  }, []);
+
+  // Approach 2: using async/await
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.get(1);
+        setError('');
+        setProjects(data);
+      }
+       catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+        } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, []);
 
   const saveProject = (project: Project) => {
     //console.log('Saving Project: ', project);
-      let updatedProjects = projects.map((p: Project) => {
+    const updatedProjects = projects.map((p: Project) => {
       return p.id === project.id ? project : p;
     });
     setProjects(updatedProjects);
   }
 
-  return<>
-  <h1>Projects</h1>
-  {/* <pre>{JSON.stringify(MOCK_PROJECTS,null,' ')}</pre> */}
-  {/* //<ProjectList projects={MOCK_PROJECTS} onSave={saveProject}/> */}
-  <ProjectList onSave={saveProject} projects={projects} />
-  </> ;
+  return <>
+    <h1>Projects</h1>
+    {/* <pre>{JSON.stringify(MOCK_PROJECTS,null,' ')}</pre> */}
+    {/* //<ProjectList projects={MOCK_PROJECTS} onSave={saveProject}/> */}
+
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
+
+
+    <ProjectList onSave={saveProject} projects={projects} />
+
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
+  </>;
 }
 
 export default ProjectsPage;
