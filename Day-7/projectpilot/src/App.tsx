2e6a7f15575781a48074a84cc6000cd94a7548f1
Gerardo Soto <soto1187@gmail.com>
2025-05-26 09:11:04 -0700

    Lab 21: Route Parameters

--- Day-7/projectpilot/src/App.tsx
+++ Day-7/projectpilot/src/App.tsx
@@ -4,6 +4,7 @@
 import { BrowserRouter, NavLink, Route, Routes } from "react-router";
 import ProjectsPage from "./projects/ProjectsPage";
 import HomePage from "./Home/HomePage";
+import ProjectPage from "./projects/ProjectPage";
 
 function App() {
   // return (
@@ -35,6 +36,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects/:id" element={<ProjectPage />} />
         </Routes>
       </div>
     </BrowserRouter>
