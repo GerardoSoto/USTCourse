c0d138fb85f7b73430c7911271b6c3c4ceba6449
Gerardo Soto <soto1187@gmail.com>
2025-05-26 08:55:42 -0700

    Lab 20: Router Basics

--- Day-7/projectpilot/src/App.tsx
+++ Day-7/projectpilot/src/App.tsx
@@ -1,18 +1,44 @@
 // import Greeter from "./Greeter/Greter";
 //import Hello from "./Function-Component/Hello";
 // import Hello from "./Class-coponent/Hello";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router";
 import ProjectsPage from "./projects/ProjectsPage";
+import HomePage from "./Home/HomePage";
 
 function App() {
-  return (
-    //Agrega nuevos componentes
-   <div className="container">
-     <ProjectsPage />
-     {/* <Greeter first="Gerardo" last="Soto"/> */}
-     {/* <Hello name="David" enthusiasmLevel={3}></Hello> */}
+  // return (
+  //   //Agrega nuevos componentes
+  //  <div className="container">
+  //    <ProjectsPage />
+  //    {/* <Greeter first="Gerardo" last="Soto"/> */}
+  //    {/* <Hello name="David" enthusiasmLevel={3}></Hello> */}
      
-   </div>
-   );
+  //  </div>
+  //  );
+
+  return (
+    <BrowserRouter>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/"  className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
