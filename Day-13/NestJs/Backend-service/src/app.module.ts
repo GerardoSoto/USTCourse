ffd349d2bc597228b4f158c01379559b359f078b
Gerardo Soto <soto1187@gmail.com>
2025-05-30 12:35:59 -0700

    Modules for Auth added

--- Day-13/NestJs/Backend-service/src/app.module.ts
+++ Day-13/NestJs/Backend-service/src/app.module.ts
@@ -5,10 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import {ProjectSchema } from './Schema/project.schema'
 import { AppServiceDB } from './AppDB.service';
 import { LoggerMiddleware } from './Middleware/loggerMiddleware';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017',{dbName: 'projectdb'}),
-    MongooseModule.forFeature([{name: 'Project', schema: ProjectSchema}])
+    MongooseModule.forFeature([{name: 'Project', schema: ProjectSchema}]),
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService, AppServiceDB],
